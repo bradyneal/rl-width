35dea40b0eb613fc0844dca7dbe684c6b77eac4d
Brady Neal <bradyneal11@gmail.com>
2019-04-30 21:42:20 -0400

    Add plot saving, colors, linestyles, and script code for running plotting

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from pprint import pprint
 
 import gym
 from train import zoo_train, STR_TO_ALGO
+#from results_plotting import plot_all_widths
 
 
 RL_BASELINES_ZOO_HYPER = 'rl-baselines-zoo'
@@ -23,8 +24,28 @@ HYPERPARAM_CHOICES = [RL_BASELINES_ZOO_HYPER, RLLIB_HYPER, DEFAULT_HYPER]
 RESULTS_DEF = os.getcwd()
 LOG_DEF = 'logs'
 EXP_DEF = 'general'
+FIG_DEF = 'figures'
 HYPERPARAM_DEF = RL_BASELINES_ZOO_HYPER
 DEPTH_DEF = 2
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('--results-dir', default=RESULTS_DEF, type=str, help='path to where all results are written')
+PARSER.add_argument('--log-dir', default=LOG_DEF, type=str, help='log folder for CSV logs, saved models, and hyperparameter settings')
+PARSER.add_argument('--name', default=EXP_DEF, type=str, help='name of experiment')
+PARSER.add_argument('--figure-dir', default=FIG_DEF, type=str, help='folder for figures of experiments')
+PARSER.add_argument('--env', nargs='+', default=['CartPole-v1'], type=str, help='environment ID(s)')
+PARSER.add_argument('--algo', nargs='+', default=['ppo2'], type=str, choices=list(STR_TO_ALGO.keys()), help='RL Algorithm')
+PARSER.add_argument('-s', '--start-end-seed', nargs=2, type=int, help='first and last (inclusive) random seeds')
+PARSER.add_argument('--n-seeds', default=1, type=int, help='number of random seeds to run')
+PARSER.add_argument('-w', '--widths', nargs='+', default=[64], type=int, help='network width(s)')
+PARSER.add_argument('--hyperparam', default=HYPERPARAM_DEF, type=str, choices=HYPERPARAM_CHOICES, help='hyperparameter settings')
+    
+PARSER.add_argument('-d', '--depth', default=DEPTH_DEF, type=int, help='number of hidden layers')
+PARSER.add_argument('-n', '--n-timesteps', help='Overwrite the number of timesteps', default=-1, type=int)
+#parser.add_argument('-tb', '--tensorboard-dir', default='tensorboard', type=str, help='Tensorboard log dir')
+PARSER.add_argument('--log-interval', help='Override log interval (default: -1, no change)', default=-1, type=int)
+PARSER.add_argument('--scale-lr', action='store_true', help='scale learning rate with width')
+PARSER.add_argument('--no-tensorboard', action='store_true', help='do not log tensorboard events files (logged by default)')
 
 
 def check_envs_valid(env_ids):
@@ -38,34 +59,21 @@ def check_envs_valid(env_ids):
 
 
 def build_log_dir(env_id, algo, width, seed, results_dir=RESULTS_DEF, log_folder=LOG_DEF, exp_name=EXP_DEF, hyperparam_setting=HYPERPARAM_DEF, scale_lr=False, depth=DEPTH_DEF):
-    if scale_lr:
-        algo_dir = '{}_{}_scale-lr'.format(algo, hyperparam_setting)
-    else:
-        algo_dir = '{}_{}'.format(algo, hyperparam_setting)
+    algo_dir = get_algo_fullname(algo, hyperparam_setting, scale_lr)
     log_dir = os.path.join(results_dir, log_folder, exp_name, env_id, algo_dir, 
                            'w{}_d{}'.format(width, depth), 'seed{}'.format(seed))
     return log_dir
 
+def get_algo_fullname(algo, hyperparam_setting, scale_lr):
+    if scale_lr:
+        algo_fullname = '{}_{}_scale-lr'.format(algo, hyperparam_setting)
+    else:
+        algo_fullname = '{}_{}'.format(algo, hyperparam_setting)
+    return algo_fullname
+
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--results-dir', default=RESULTS_DEF, type=str, help='path to where all results are written')
-    parser.add_argument('-l', '--log-dir', default=LOG_DEF, type=str, help='Log dir for CSV logs, saved models, and hyperparameter settings')
-    parser.add_argument('--name', default=EXP_DEF, type=str, help='name of experiment')
-    parser.add_argument('--env', nargs='+', default=['CartPole-v1'], type=str, help='environment ID(s)')
-    parser.add_argument('--algo', nargs='+', default=['ppo2'], type=str, choices=list(STR_TO_ALGO.keys()), help='RL Algorithm')
-    parser.add_argument('-s', '--start-end-seed', nargs=2, default=[0, 0], type=int, help='first and last (inclusive) random seeds')
-    parser.add_argument('-w', '--widths', nargs='+', default=[64], type=int, help='network width(s)')
-    parser.add_argument('--hyperparam', default=HYPERPARAM_DEF, type=str, choices=HYPERPARAM_CHOICES, help='hyperparameter settings')
-    
-    parser.add_argument('-d', '--depth', default=DEPTH_DEF, type=int, help='number of hidden layers')
-    parser.add_argument('-n', '--n-timesteps', help='Overwrite the number of timesteps', default=-1, type=int)
-    #parser.add_argument('-tb', '--tensorboard-dir', default='tensorboard', type=str, help='Tensorboard log dir')
-    parser.add_argument('--log-interval', help='Override log interval (default: -1, no change)', default=-1, type=int)
-    parser.add_argument('--scale-lr', action='store_true', help='scale learning rate with width')
-    parser.add_argument('--no-tensorboard', action='store_true', help='do not log tensorboard events files (logged by default)')
-    
-    args = parser.parse_args()
+    args = PARSER.parse_args()
     args_dict = vars(args)
     print('Effective command line arguments:')
     pprint(args_dict)
@@ -74,16 +82,22 @@ def main():
     exp_name = args.name
     env_ids = args.env
     algos = args.algo
-    start_seed, end_seed = args.start_end_seed
+    if args.start_end_seed is None:
+        start_seed = 0
+        end_seed = args.n_seeds
+    else:
+        start_seed, end_seed = args.start_end_seed
     widths = args.widths
     hyperparam_setting = args.hyperparam
     results_dir = args.results_dir
     log_folder = args.log_dir
+    figure_dir = os.path.join(results_dir, args.figure_dir)
     
     # Create dictionary for passing remaining named arguments to function
     remain_args_dict = {k: v for k, v in args_dict.items() if k not in
                         set(['env', 'algo', 'start_end_seed', 'widths',
-                             'hyperparam', 'results_dir', 'log_dir', 'name'])}
+                             'hyperparam', 'results_dir', 'log_dir', 'name',
+                             'figure_dir'])}
     
     # Check if environments are valid and run experiments
     check_envs_valid(env_ids)
@@ -98,6 +112,8 @@ def main():
                         raise NotImplementedError("To be implemented")
                     elif hyperparam_setting == DEFAULT_HYPER:
                         raise NotImplementedError("To be implemented")
+#            if start_seed == 0:
+#                plot_all_widths(env_id, algo, widths, end_seed + 1, figure_dir)
 
        
 if __name__ == '__main__':
--- results_plotting.py
+++ results_plotting.py
@@ -6,21 +6,44 @@ Created on Mon Apr 29 14:57:36 2019
 @author: brady
 """
 
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt
+from collections import OrderedDict
 
 from stable_baselines.bench.monitor import load_results
 from stable_baselines.results_plotter import \
     ts2xy, window_func, X_TIMESTEPS, X_EPISODES, X_WALLTIME
 
-from main import build_log_dir
+from main import build_log_dir, get_algo_fullname, PARSER, EXP_DEF, HYPERPARAM_DEF 
 from train import build_monitor_dir
 
 
 # 0 corresponds to implementing np.vstack code, whereas
 # 1 correpsonds to implementing np.hstack code
 STACK_DIM = 0
+LINESTYLES = ['-', '--', '-.', ':']
+# https://matplotlib.org/gallery/lines_bars_and_markers/linestyles.html
+FANCY_LINESTYLES = OrderedDict(
+    [('solid',               (0, ())),
+#     ('loosely dotted',      (0, (1, 10))),
+     ('dotted',              (0, (1, 5))),
+     ('densely dotted',      (0, (1, 1))),
+
+#     ('loosely dashed',      (0, (5, 10))),
+     ('dashed',              (0, (5, 5))),
+     ('densely dashed',      (0, (5, 1))),
+
+#     ('loosely dashdotted',  (0, (3, 10, 1, 10))),
+#     ('dashdotted',          (0, (3, 5, 1, 5))),
+#     ('densely dashdotted',  (0, (3, 1, 1, 1))),
+
+#     ('loosely dashdotdotted', (0, (3, 10, 1, 10, 1, 10))),
+#     ('dashdotdotted',         (0, (3, 5, 1, 5, 1, 5))),
+#     ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))
+     ])
+COLORS = []
 
 
 def plot_all_widths(*args, **kwargs):
@@ -30,14 +53,18 @@ def plot_all_widths(*args, **kwargs):
 
 class ResultsPlotter:
     
-    def __init__(self, env_id, algo, widths, num_seeds, path_args={},
-                 smooth_window=50, smooth_seeds=True, smooth_mean=False,
+    def __init__(self, env_id, algo, widths, num_seeds, figure_dir,
+                 hyperparam_setting=HYPERPARAM_DEF, scale_lr=False,
+                 path_args={}, smooth_window=50, smooth_seeds=True, smooth_mean=False,
                  smooth_std=False, xaxis=X_EPISODES, conf_int='mean',
-                 alpha=0.5, trim_diff_widths=True):
+                 alpha=0.5, trim_diff_widths=True, linestyle='-'):
         self.env_id = env_id
         self.algo = algo
         self.widths = widths
         self.num_seeds = num_seeds
+        self.figure_dir = figure_dir
+        self.hyperparam_setting = hyperparam_setting
+        self.scale_lr = scale_lr
         self.path_args = path_args
         self.smooth_window = smooth_window
         self.smooth_seeds = smooth_seeds
@@ -47,6 +74,7 @@ class ResultsPlotter:
         self.conf_int = conf_int
         self.alpha = alpha
         self.trim_diff_widths = trim_diff_widths
+        self.linestyle = linestyle
         
     def get_monitor_dir(self, width, seed):
         log_dir = build_log_dir(env_id=self.env_id, algo=self.algo,
@@ -133,18 +161,45 @@ class ResultsPlotter:
         return xs, means, stds
     
     def plot_all_widths(self):
+        if self.linestyle == 'fancy':
+            linestyles = list(FANCY_LINESTYLES.values())
+        elif self.linestyle == 'normal':
+            linestyles = LINESTYLES
+        elif self.linestyle in LINESTYLES:
+            linestyles = [self.linestyle]
+        else:
+            raise ValueError('Invalid linestyle:', self.linestyle)
+        
         widths = self.widths
         xs, y_avgs, y_offsets = self.get_all_widths_mean_and_std()
         assert len(xs) == len(widths)
         
-        plt.figure()
-        for width, x, y, y_offset in zip(widths, xs, y_avgs, y_offsets):
-            plt.plot(x, y, '-', label=str(width))
-            plt.fill_between(x, y + y_offset, y - y_offset, alpha=self.alpha)
+        fig = plt.figure()
+        for i, (width, x, y, y_offset) in enumerate(zip(widths, xs, y_avgs, y_offsets)):
+            color = 'C' + str(i)
+            linestyle = linestyles[i % len(linestyles)]
+            plt.plot(x, y, '-', label=str(width), color=color, linestyle=linestyle)
+            plt.fill_between(x, y + y_offset, y - y_offset, facecolor=color, alpha=self.alpha)
         plt.legend()
         plt.xlabel(self.xaxis)
         plt.ylabel('average return')
         plt.tight_layout()
-
+        
+        os.makedirs(self.figure_dir, exist_ok=True)
+        algo_fullname = get_algo_fullname(self.algo, self.hyperparam_setting, self.scale_lr)
+        filename = '{}_{}.pdf'.format(self.env_id, algo_fullname)
+        path = os.path.join(self.figure_dir, filename)
+        print('Saving figure to', path)
+        fig.savefig(path, bbox_inches='tight')
+        
     def get_all_widths_est_bootstrap(self):
         raise NotImplementedError("To be implemented")
+        
+if __name__ == '__main__':
+    args = PARSER.parse_args()    
+    for env_id in args.env:
+        for algo in args.algo:
+            figure_dir = os.path.join(args.results_dir, args.figure_dir, args.name)
+            plot_all_widths(env_id, algo, args.widths, num_seeds=args.n_seeds, figure_dir=figure_dir,
+                 hyperparam_setting=args.hyperparam, scale_lr=args.scale_lr)
+            
